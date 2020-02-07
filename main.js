97f3aee0037d72ede390e07dbc04414353d6c8a6
Gitosthenese <abledso3@uw.edu>
2020-02-06 18:50:08 -0800

    add audio toggle

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let ASSET_MANAGER = new AssetManager();
+let ON_TITLESCREEN = true;
+
+function IsOnTitleScreen() { return ON_TITLESCREEN; }
 
 //! ******** Animation Definition ******** */
 function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
@@ -51,7 +54,6 @@ function Background(game, spritesheet) {
     this.spritesheet = spritesheet;
     this.game = game;
     this.ctx = game.ctx;
-    this.titleScreenComp = true;
 };
 
 Background.prototype.draw = function () {
@@ -60,15 +62,56 @@ Background.prototype.draw = function () {
 
 Background.prototype.update = function () {
     if (this.game.userInput.includes(' ')) {
-        if(this.titleScreenComp) {
+        if(IsOnTitleScreen()) {
             this.spritesheet = ASSET_MANAGER.getAsset("./res/map/proto_map.jpg");
             document.getElementById('audio').play();
             document.getElementById('audio').volume = 0.5;
-            this.titleScreenComp = false;
+            ON_TITLESCREEN = false;
         }
     }
 };
 
+//! ******** Volume Toggle Definition ******** */
+function VolumeToggle(game, spritesheet) {
+    this.audio = document.getElementById('audio');
+    this.spritesheet = spritesheet;
+    this.ctx = game.ctx;
+    this.state = 'on';
+}
+
+VolumeToggle.prototype.update = function () {};
+
+/**
+ * Draws the volume on/off icon depending on state.
+ * @see https://webplatform.github.io/docs/concepts/programming/drawing_images_onto_canvas/
+ *      for overloaded drawImage() parameters
+ */
+VolumeToggle.prototype.draw = function () {
+    if (!IsOnTitleScreen()) {
+        if (this.state == 'on') {
+            this.ctx.drawImage(this.spritesheet, 48, 0, 47 ,47, 5, 5, 30, 30);
+        } else if (this.state == 'off') {
+            this.ctx.drawImage(this.spritesheet, 0, 0, 47 ,47, 5, 5, 30, 30);
+        }
+    }
+};
+
+/**
+ * Toggles volume state to ON if not playing; sets it to OFF and restarts audio track if it is playing.
+ */
+VolumeToggle.prototype.flipVolume = function () {
+    if(!IsOnTitleScreen()) {
+        let isON = this.state == 'on' ? true : false;
+        if (isON) {
+            this.state = 'off';
+            this.audio.pause();
+            this.audio.currentTime = 0;
+        } else {
+            this.state = 'on'
+            this.audio.play();
+        }
+    }
+}
 
 //! ******** Skeleton Dagger Sprite Definition ******** */
 function SkeletonDagger(game, spritesheet) {
@@ -197,14 +240,18 @@ MaleKnightMace.prototype.draw = function() {
 }
 
 
-//! ******** Retrieve Assets & Start Game ******** */
-ASSET_MANAGER.queueDownload("./res/map/proto_map.jpg");
-ASSET_MANAGER.queueDownload("./res/character/skeleton_dagger.png");
-ASSET_MANAGER.queueDownload("./res/audio/megalovania.mp3");
+//! ******** QUEUE ASSET DOWNLOAD ******** */
+// Background images
 ASSET_MANAGER.queueDownload("./res/map/titlescreen.jpg");
+ASSET_MANAGER.queueDownload("./res/map/proto_map.jpg");
+// Character sprites
+ASSET_MANAGER.queueDownload("./res/character/skeleton_dagger.png");
 ASSET_MANAGER.queueDownload("./res/character/male_knight_spear.png");
 ASSET_MANAGER.queueDownload("./res/character/male_knight_mace.png");
-
+// Audio assets
+ASSET_MANAGER.queueDownload("./res/audio/megalovania.mp3");
+ASSET_MANAGER.queueDownload("./res/audio/volume_bgON.png");
+ASSET_MANAGER.queueDownload("./res/audio/volume_bgOFF.png");
 
 
 ASSET_MANAGER.downloadAll(function () {
@@ -212,8 +259,6 @@ ASSET_MANAGER.downloadAll(function () {
     var ctx = canvas.getContext('2d');
 
     var gameEngine = new GameEngine();
-
-    console.log('starting audio');
 
     gameEngine.init(ctx);
     gameEngine.start();
@@ -223,4 +268,8 @@ ASSET_MANAGER.downloadAll(function () {
     gameEngine.addEntity(new MaleKnightSpear(gameEngine, ASSET_MANAGER.getAsset("./res/character/male_knight_spear.png")));
     gameEngine.addEntity(new MaleKnightMace(gameEngine, ASSET_MANAGER.getAsset("./res/character/male_knight_mace.png")));
     gameEngine.addEntity(new SkeletonDagger(gameEngine, ASSET_MANAGER.getAsset("./res/character/skeleton_dagger.png")));
+
+    let volumeToggle = new VolumeToggle(gameEngine, ASSET_MANAGER.getAsset("./res/audio/volume_bgON.png"));
+    gameEngine.setVolumeToggle(volumeToggle);
+    gameEngine.addEntity(volumeToggle);
 });
