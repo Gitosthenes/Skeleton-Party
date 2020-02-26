4255691b1709582a327a46401db8d6fc20054239
bbMorris <blm24@uw.edu>
2020-02-25 22:55:20 -0800

    Starting enemy refactor.

--- main.js
+++ main.js
@@ -219,7 +219,7 @@ function entityAnimationInit(entity, spritesheet, type) {
 
 SkeletonDagger.prototype.takeDamage = function(amount) {
     hp -= amount;
-}
+};
 
 SkeletonDagger.prototype.update = function () {
     // let that = this;
@@ -262,116 +262,6 @@ SkeletonDagger.prototype.draw = function () {
     }
 
 };
-
-function MaleKnightSpear(game,spritesheet) {
-
-    this.enemyHP = 1000;
-    this.x = this.relativeX = 0;
-    this.y = this.relativeY = 0;
-    this.hitboxOffsetX = 18;
-    this.hitboxOffsetY = 10;
-    this.safeDist = 63;
-    this.speed = 200;
-    this.game = game;
-    this.ctx = game.ctx;
-    this.isAttacking = false;
-    this.isRecoiling = false;
-    this.direction = 'Down';
-    this.state = "walkDown";
-    this.attAnimationSpeed = 0.12;
-    entityAnimationInit(this, spritesheet, 2);
-    this.currAnimation = this.animations[this.state];
-    setEnemyRandomLocation(this);
-    console.log("Spear spawn X " + this.x + "Y " + this.y);
-    Entity.call(game, this.x, this.y, undefined);
-    this.hitbox = new Hitbox(this.x, this.y, 55, 30, true);
-    this.hurtbox = new Hitbox(0, 0, 0, 0, false);
-}
-
-MaleKnightSpear.prototype.update = function() {
-    if(!ON_TITLESCREEN) {
-        let animationDelay = this.currAnimation.totalTime / 1.8;
-
-        updateEnemyPositionAndAnimation(this);
-        updateHitbox(this, (this.x + this.hitboxOffsetX), (this.y + this.hitboxOffsetY));
-        updateInvincibilityFrames(this);
-
-        if(this.isAttacking && this.currAnimation.elapsedTime > animationDelay) activateHurtbox(this);
-        if(!this.isAttacking) this.hurtbox.isActive = false;
-        checkForCollisions(this);
-
-        if (this.isRecoiling && this.hitByEnemy) {
-            this.enemyHP -= atk;
-            if(this.enemyHP <= 0) {
-                this.removeFromWorld = true;
-            }
-        }
-    }
-    Entity.prototype.update.call(this);
-}
-
-MaleKnightSpear.prototype.draw = function() {
-    if(!ON_TITLESCREEN && !GAME_OVER) {
-        this.currAnimation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
-        Entity.prototype.draw.call(this);
-    }
-}
-
-function MaleKnightMace(game, spritesheet) {
-
-    this.enemyHP = 1000;
-    this.enemyAttack= 1;
-    this.x = this.relativeX = 850;
-    this.y = this.relativeY = 80;
-    this.hitboxOffsetX = 18;
-    this.hitboxOffsetY = 10;
-    this.safeDist = 60;
-    this.speed = 150;
-    this.game = game;
-    this.ctx = game.ctx;
-    this.isAttacking = false;
-    this.isRecoiling = false;
-    this.direction = 'Down';
-    this.state = "walkDown";
-
-    this.attAnimationSpeed = 0.17;
-    entityAnimationInit(this, spritesheet, 1);
-    this.currAnimation = this.animations[this.state];
-    setEnemyRandomLocation(this, this.currAnimation.frameWidth);
-    Entity.call(game, this.x, this.y, undefined);
-    this.hitbox = new Hitbox(this.x, this.y, 60, 40, true);
-    this.hurtbox = new Hitbox(0, 0, 0, 0, false);
-}
-
-MaleKnightMace.prototype.update = function() {
-    if(!ON_TITLESCREEN) {
-        let animationDelay = this.currAnimation.totalTime / 1.8;
-
-        updateEnemyPositionAndAnimation(this);
-        updateHitbox(this, (this.x + this.hitboxOffsetX), (this.y + this.hitboxOffsetY));
-        updateInvincibilityFrames(this);
-
-        if(this.isAttacking && this.currAnimation.elapsedTime > animationDelay) activateHurtbox(this);
-        if(!this.isAttacking) this.hurtbox.isActive = false;
-        checkForCollisions(this);
-
-        if (this.isRecoiling && this.hitByEnemy) {
-            this.enemyHP -= atk;
-            if(this.enemyHP <= 0) {
-                this.removeFromWorld = true;
-            }
-        }
-    }
-    Entity.prototype.update.call(this);
-};
-
-MaleKnightMace.prototype.draw = function() {
-    if(!ON_TITLESCREEN && !GAME_OVER) {
-        this.currAnimation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
-        Entity.prototype.draw.call(this);
-    }
-};
-
 
 //UI stuff below
 function SkeletonHealthUI(game, spritesheet) {
